b0c541633e890b9e58d479eb0cc098529a8f753d
slavcheww <47758510+slavcheww@users.noreply.github.com>
2020-01-23 16:43:10 +0200

    Update Questions.cs

--- Fighter/Assets/Scripts/UI/Questions.cs
+++ Fighter/Assets/Scripts/UI/Questions.cs
@@ -1,12 +1,36 @@
-﻿using System.IO;
+﻿using UnityEngine;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Text;
+using UnityEngine.UI;
 
 public class Questions : MonoBehaviour
 {
     string filePath = "";
-    StreamReader reader = new StreamReader(filePath, Encoding.GetEncoding("windows-1251"));
+    var[] lines = new StreamReader(filePath, Encoding.GetEncoding("windows-1251"));
+    string question = "";
+
+    public void ReadFile(string[] lines, string question)
+    {
+        int i = 0;
+        foreach (var item in lines)
+        {
+            if (i == 0)
+            {
+                question = item;
+            }
+            i++;
+        }
+    }
     
+    void Start()
+    {
+       
+        ReadFile(lines,question);
+
+       
+    }
+
+
 }
