c80e5c24ff30acf9fe9712446755fbd592fdec10
Iulian <uliandashev@abv.bg>
2020-02-19 22:05:34 +0200

    Pochti na finala sme 2

--- Fighter/Assets/Scripts/Enemies/FollowPlayer.cs
+++ Fighter/Assets/Scripts/Enemies/FollowPlayer.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
-    public Boss boss;
+    public Rigidbody2D rb2d;
 
     public float speed = 10f;
+    public float attackSpeed;
 
     private void FixedUpdate()
     {
-        Vector2 target = new Vector2(player.position.x, transform.position.y);
+        if (player.position.x >= transform.position.x)
+        {
+            rb2d.velocity = -transform.right * attackSpeed;
+        }
+        else
+        {
+            Vector2 target = new Vector2(player.position.x, transform.position.y);
 
-        
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.fixedDeltaTime);
-
-        transform.position = this.transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.fixedDeltaTime);
+        }
     }
 }
